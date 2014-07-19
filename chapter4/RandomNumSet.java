37f3e5b89cf9c51a11f0ac04985b747740bac2d2
Ruiyuan Li <lry198010@gmail.com>
2014-07-19 20:49:51 +0800

    another implementation of selected random numbers from numbers in 1 to 49, which was an assign test in Chapter3

---
+++ chapter4/RandomNumSet.java
@@ -0,0 +1,26 @@
+import static java.lang.Math.*;
+public class RandomNumSet {
+  public static void main (String[] args){
+    int[] iSelectedNum;
+    int iNumOfRolls = 5;
+    for (int i=1; i < iNumOfRolls; i++){
+      int iCount = 0;
+      iSelectedNum = new int [6];
+      LabelDo:
+      do {
+        int iOneRandom = (int)(random()*49 + 1);
+	for (int j = 0; j < iCount; j++){
+          if (iOneRandom == iSelectedNum[j]) {
+            System.out.println(iOneRandom + " is identity to:" + j);
+            continue LabelDo;
+          } 
+	}
+        iSelectedNum[iCount++] = iOneRandom; 
+      }while (iCount < iSelectedNum.length);
+      for (int iNum:iSelectedNum){
+        System.out.println(iNum);
+      } 
+      System.out.println();
+    }
+  }
+}
