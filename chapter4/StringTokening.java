f6e43fb5c9a8c27f17e9b75fb652b849555362b1
Ruiyuan Li <lry198010@gmail.com>
2014-07-21 23:45:28 +0800

    finished coding example of StringTokening.java

---
+++ chapter4/StringTokening.java
@@ -0,0 +1,16 @@
+public class StringTokening {
+  public static void main (String[] args) {
+    String sText = "To be or not to be,,,, that is the question";
+    String sDelimiters = "[,. ]+";
+    int[] iaLimits = {0,-1};
+    
+    for (int iLimit : iaLimits) {
+      System.out.println("Using limit:" + iLimit);
+      String[] saTokens = sText.split(sDelimiters,iLimit);
+      for (String sToken : saTokens){
+        System.out.println(sToken + "::::");
+      }
+      System.out.println();
+    }
+  }
+}
