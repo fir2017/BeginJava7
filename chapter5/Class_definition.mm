46d1fa77215a490f7b2905ff9b3bbd176effd85b
Ruiyuan Li <lry198010@gmail.com>
2014-07-31 18:13:43 +0800

    create mind mapping for Chapter5 using FreeMind software, named as Class_definition.mm

---
+++ chapter5/Class_definition.mm
@@ -0,0 +1,35 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1406792073979" ID="ID_594809272" MODIFIED="1406793820604" TEXT="&#x7b2c;5&#x7ae0;&#xff1a;&#x5b9a;&#x4e49;&#x7c7b;">
+<node CREATED="1406792510964" HGAP="11" ID="ID_1898257442" MODIFIED="1406792939412" POSITION="right" STYLE="bubble" TEXT="&#x7c7b;&#x7684;&#x5b9a;&#x4e49;" VSHIFT="-36">
+<node CREATED="1406793001833" ID="ID_1467918274" MODIFIED="1406793011065" TEXT="&#x57df;">
+<node CREATED="1406793039212" HGAP="35" ID="ID_6643145" MODIFIED="1406793400041" TEXT="&#x9759;&#x6001;&#x57df;/&#x7c7b;&#x53d8;&#x91cf;">
+<node CREATED="1406793284591" ID="ID_1952083707" MODIFIED="1406793392541" TEXT="&#x4fdd;&#x5b58;&#x5728;&#x7c7b;&#x7a7a;&#x95f4;&#xff0c;&#x901a;&#x8fc7;&#x7c7b;&#x540d;[&#x5b9e;&#x4f8b;&#x53d8;&#x91cf;&#x540d;].&#x53d8;&#x91cf;&#x540d;&#x8bbf;&#x95ee;"/>
+</node>
+<node CREATED="1406793126760" HGAP="22" ID="ID_1611982343" MODIFIED="1406793266748" TEXT="&#x975e;&#x9759;&#x6001;&#x57df;/&#x5b9e;&#x4f8b;&#x53d8;&#x91cf;" VSHIFT="1">
+<node CREATED="1406793324843" ID="ID_598437735" MODIFIED="1406793360893" TEXT="&#x4fdd;&#x5b58;&#x5728;&#x5b9e;&#x4f8b;&#x7a7a;&#x95f4;&#xff0c;&#x901a;&#x8fc7;&#x5b9e;&#x4f8b;&#x53d8;&#x91cf;&#x540d;.&#x53d8;&#x91cf;&#x540d;&#x8bbf;&#x95ee;"/>
+</node>
+<node CREATED="1406793677127" HGAP="24" ID="ID_409491206" MODIFIED="1406793712525" TEXT="Final &#x57df;&#xff1a;&#x5176;&#x521d;&#x59cb;&#x503c;&#x5728;&#x8fd0;&#x884c;&#x671f;&#x95f4;&#x4e0d;&#x80fd;&#x6539;&#x53d8;" VSHIFT="2"/>
+</node>
+<node CREATED="1406793024565" HGAP="19" ID="ID_946607372" MODIFIED="1406793250724" TEXT="&#x65b9;&#x6cd5;" VSHIFT="13">
+<node CREATED="1406793150242" HGAP="36" ID="ID_170240377" MODIFIED="1406793475272" TEXT="&#x9759;&#x6001;&#x65b9;&#x6cd5;/&#x7c7b;&#x65b9;&#x6cd5;">
+<node CREATED="1406793284591" HGAP="37" ID="ID_1197592711" MODIFIED="1406793473475" TEXT="&#x4fdd;&#x5b58;&#x5728;&#x7c7b;&#x7a7a;&#x95f4;&#xff0c;&#x901a;&#x8fc7;&#x7c7b;&#x540d;[&#x5b9e;&#x4f8b;&#x53d8;&#x91cf;&#x540d;].&#x65b9;&#x6cd5;&#x540d;&#x8bbf;&#x95ee;" VSHIFT="-1"/>
+<node CREATED="1406797850618" HGAP="40" ID="ID_737345686" MODIFIED="1406797915023" TEXT="&#x5728;&#x7c7b;&#x65b9;&#x6cd5;&#x5185;&#xff0c;&#x53ea;&#x80fd;&#x8c03;&#x7528;&#x7c7b;&#x65b9;&#x6cd5;&#xff0c;&#x4f7f;&#x7528;&#x7c7b;&#x53d8;&#x91cf;" VSHIFT="-2"/>
+</node>
+<node CREATED="1406793191948" ID="ID_16831297" MODIFIED="1406793210933" TEXT="&#x975e;&#x9759;&#x6001;&#x65b9;&#x6cd5;/&#x5b9e;&#x4f8b;&#x65b9;&#x6cd5;">
+<node CREATED="1406793324843" ID="ID_578144538" MODIFIED="1406793446935" TEXT="&#x4fdd;&#x5b58;&#x5728;&#x5b9e;&#x4f8b;&#x7a7a;&#x95f4;&#xff0c;&#x901a;&#x8fc7;&#x5b9e;&#x4f8b;&#x53d8;&#x91cf;&#x540d;.&#x65b9;&#x6cd5;&#x540d;&#x8bbf;&#x95ee;"/>
+</node>
+</node>
+</node>
+<node CREATED="1406792967561" ID="ID_1775261969" MODIFIED="1406793453999" POSITION="right" STYLE="bubble" TEXT="&#x5b9a;&#x4e49;&#x7c7b;">
+<node CREATED="1406793737218" ID="ID_537816572" MODIFIED="1406793768740" TEXT="&#x7c7b;&#x540d;&#x4e0e;&#x6587;&#x4ef6;&#x540d;&#x4e00;&#x6837;&#xff0c;&#x7b2c;&#x4e00;&#x4e2a;&#x5b57;&#x6bcd;&#x901a;&#x5e38;&#x5927;&#x5199;"/>
+<node CREATED="1406793771664" ID="ID_1809921986" MODIFIED="1406793788214" TEXT="&#x4ee5;class&#x5173;&#x952e;&#x5b57;&#x5b9a;&#x4e49;"/>
+<node CREATED="1406801048217" ID="ID_1903947374" MODIFIED="1406801092426" TEXT="&#x7c7b;&#x5b9a;&#x4e49;&#x7684;&#x683c;&#x5f0f; class ClassName {body:field and methods}"/>
+</node>
+<node CREATED="1406792538903" HGAP="23" ID="ID_1688566308" MODIFIED="1406792938194" POSITION="right" STYLE="bubble" TEXT="&#x5b9a;&#x4e49;&#x65b9;&#x6cd5;" VSHIFT="23">
+<node CREATED="1406801098443" ID="ID_511275424" MODIFIED="1406801175980" TEXT="&#x65b9;&#x6cd5;&#x7684;&#x7ed3;&#x6784;&#x683c;&#x5f0f;&#xff1a;ruturn_type methodName ( arg1, arg2, ..., argn){ code block}"/>
+<node CREATED="1406801185372" ID="ID_1126697043" MODIFIED="1406801211225" TEXT="&#x65b9;&#x6cd5;&#x7684;&#x8fd4;&#x56de;&#x503c;"/>
+</node>
+<node CREATED="1406792983793" HGAP="31" ID="ID_1584345120" MODIFIED="1406793464031" POSITION="right" STYLE="bubble" TEXT="&#x6784;&#x9020;&#x51fd;&#x6570;" VSHIFT="20"/>
+</node>
+</map>
