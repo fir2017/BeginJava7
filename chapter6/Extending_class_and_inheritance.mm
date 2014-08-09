9cadc8d8f3c638d0178ab9483fd7db62f6c0f2af
Ruiyuan Li <lry198010@gmail.com>
2014-08-09 22:34:15 +0800

    add Extending_Class_and_inheritance.mm for chapter6, add node about (using existing class) in to the mm file

---
+++ chapter6/Extending_class_and_inheritance.mm
@@ -0,0 +1,74 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1407591979087" ID="ID_40349216" MODIFIED="1407592817701" TEXT="Extending class and inheritance">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        &#22522;&#20110;&#24050;&#26377;&#31867;&#23450;&#20041;&#26032;&#31867;&#24182;&#37325;&#29992;
+      </li>
+      <li>
+        &#22810;&#24577;&#20197;&#21450;&#22914;&#20309;&#23450;&#20041;&#31867;&#26469;&#21033;&#29992;&#22810;&#24577;
+      </li>
+      <li>
+        &#25277;&#35937;&#26041;&#27861;
+      </li>
+      <li>
+        &#25277;&#35937;&#31867;
+      </li>
+      <li>
+        &#25509;&#21475;&#20197;&#21450;&#22914;&#20309;&#23450;&#20041;&#33258;&#24049;&#30340;&#25509;&#21475;
+      </li>
+      <li>
+        &#22312;&#31867;&#20013;&#20351;&#29992;&#25509;&#21475;&#30340;&#26041;&#27861;
+      </li>
+      <li>
+        &#20351;&#29992;&#25509;&#21475;&#23454;&#29616;&#22810;&#24577;&#31867;&#30340;&#26041;&#27861;
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1407592199523" ID="ID_1077517048" MODIFIED="1407592301999" POSITION="right" STYLE="bubble" TEXT="using existing class">
+<node CREATED="1407593024806" ID="ID_797435119" MODIFIED="1407593085213" TEXT="derivation: define a class base on an existing class"/>
+<node CREATED="1407593094214" ID="ID_1960861555" MODIFIED="1407593187244" TEXT="drived class: is the direct subclass  of the class it derived"/>
+<node CREATED="1407593192837" ID="ID_1797183316" MODIFIED="1407593230296" TEXT="base class or super class: is the class used to derive other class"/>
+<node CREATED="1407593267498" ID="ID_375792004" MODIFIED="1407593368584" TEXT="use extends to define a class base on an existing class">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      class <font color="#e61111">derivedClass</font>&#160;extends <font color="#ee1111">baseClass</font>&#160;{
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1407592219120" ID="ID_1974189861" MODIFIED="1407592297647" POSITION="right" STYLE="bubble" TEXT="class inheritance"/>
+<node CREATED="1407592310989" ID="ID_163313151" MODIFIED="1407592324925" POSITION="right" STYLE="bubble" TEXT="the @override annotation"/>
+<node CREATED="1407592328273" ID="ID_317127975" MODIFIED="1407592376019" POSITION="right" STYLE="bubble" TEXT="Choosing base class access attributes"/>
+<node CREATED="1407592378739" ID="ID_644675008" MODIFIED="1407592388847" POSITION="right" STYLE="bubble" TEXT="polymorphism"/>
+<node CREATED="1407592404678" ID="ID_1534782015" MODIFIED="1407592676313" POSITION="right" STYLE="bubble" TEXT="multiple levels of inheritance"/>
+<node CREATED="1407592429080" ID="ID_827797602" MODIFIED="1407592676313" POSITION="right" STYLE="bubble" TEXT="abstract classes"/>
+<node CREATED="1407592450338" ID="ID_104572223" MODIFIED="1407592676312" POSITION="right" STYLE="bubble" TEXT="the universal superclass"/>
+<node CREATED="1407592506916" ID="ID_216933815" MODIFIED="1407592676312" POSITION="right" STYLE="bubble" TEXT="methods accept very number of arguments"/>
+<node CREATED="1407592541713" ID="ID_1964534730" MODIFIED="1407592676312" POSITION="right" STYLE="bubble" TEXT="casting objects"/>
+<node CREATED="1407592568173" ID="ID_92361842" MODIFIED="1407592676311" POSITION="right" STYLE="bubble" TEXT="more on enumberation"/>
+<node CREATED="1407592586489" ID="ID_61856660" MODIFIED="1407592676311" POSITION="right" STYLE="bubble" TEXT="designing classes"/>
+<node CREATED="1407592602029" ID="ID_134345668" MODIFIED="1407592676310" POSITION="right" STYLE="bubble" TEXT="using the final modifier"/>
+<node CREATED="1407592625615" ID="ID_1743051872" MODIFIED="1407592676310" POSITION="right" STYLE="bubble" TEXT="interface"/>
+<node CREATED="1407592639455" ID="ID_515488740" MODIFIED="1407592676309" POSITION="right" STYLE="bubble" TEXT="anonymous classes"/>
+</node>
+</map>
