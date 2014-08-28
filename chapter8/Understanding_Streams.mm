a604d1e523c94b309f6fe13625b9e55af65b6ef9
Ruiyuan Li <lry198010@gmail.com>
2014-08-28 21:42:19 +0800

    Begin chapter8 in MM

---
+++ chapter8/Understanding_Streams.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1409221918247" ID="ID_1218745952" MODIFIED="1409222035932" TEXT="Understanding Streams">
+<node CREATED="1409221998617" ID="ID_1093956181" MODIFIED="1409222034063" POSITION="right" STYLE="bubble" TEXT="Streams and input/output operations"/>
+<node CREATED="1409222041169" ID="ID_1336041614" MODIFIED="1409222086243" POSITION="right" STYLE="bubble" TEXT="Understanding Steams"/>
+<node CREATED="1409222058971" ID="ID_392967502" MODIFIED="1409222090916" POSITION="right" STYLE="bubble" TEXT="The classes for input and output"/>
+<node CREATED="1409222076225" ID="ID_697719747" MODIFIED="1409222094178" POSITION="right" STYLE="bubble" TEXT="The standard streams"/>
+</node>
+</map>
