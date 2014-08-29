0128c8b32edfb97264a032b05196c65c66313214
Ruiyuan Li <lry198010@gmail.com>
2014-08-29 21:36:13 +0800

    finished conten for section 1, chapter 8, in MM!

--- chapter8/Understanding_Streams.mm
+++ chapter8/Understanding_Streams.mm
@@ -1,8 +1,24 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1409221918247" ID="ID_1218745952" MODIFIED="1409222035932" TEXT="Understanding Streams">
-<node CREATED="1409221998617" ID="ID_1093956181" MODIFIED="1409222034063" POSITION="right" STYLE="bubble" TEXT="Streams and input/output operations"/>
-<node CREATED="1409222041169" ID="ID_1336041614" MODIFIED="1409222086243" POSITION="right" STYLE="bubble" TEXT="Understanding Steams"/>
+<node CREATED="1409221918247" ID="ID_1218745952" MODIFIED="1409306008558" TEXT="Understanding Streams">
+<node CREATED="1409221998617" FOLDED="true" ID="ID_1093956181" MODIFIED="1409312325553" POSITION="right" STYLE="bubble" TEXT="Streams and input/output operations">
+<node CREATED="1409312008110" ID="ID_666078933" MODIFIED="1409312059034" TEXT="The primarily input/ouput steam classes reposited in java.io packages"/>
+<node CREATED="1409312062532" FOLDED="true" ID="ID_1759583982" MODIFIED="1409312310798" TEXT="The task for input/output stream is ">
+<node CREATED="1409312107760" ID="ID_814490269" MODIFIED="1409312144694" TEXT="able to read data from keyboard"/>
+<node CREATED="1409312148501" ID="ID_625622451" MODIFIED="1409312202304" TEXT="able to write formatted output into outstream, for example System.out"/>
+<node CREATED="1409312211335" ID="ID_855110160" MODIFIED="1409312279047" TEXT="able to read and write data in file contains strings or basic types of data"/>
+<node CREATED="1409312281938" ID="ID_938566763" MODIFIED="1409312306547" TEXT="able to read and write files contain objects"/>
+</node>
+</node>
+<node CREATED="1409222041169" ID="ID_1336041614" MODIFIED="1409222086243" POSITION="right" STYLE="bubble" TEXT="Understanding Steams">
+<node CREATED="1409313923543" ID="ID_300179369" MODIFIED="1409314053588" TEXT="Stream: An abstract representation of an input or output device that is source of , or destination for, data"/>
+<node CREATED="1409314056810" ID="ID_131165373" MODIFIED="1409314213912" TEXT="In java, Stream can visulized as sequence of bytes flow into or out of the progam"/>
+<node CREATED="1409314277451" ID="ID_1821598182" MODIFIED="1409314294011" TEXT="Input/Output Steam">
+<node CREATED="1409314297411" ID="ID_1143398014" MODIFIED="1409314597281" TEXT="Outpu stream: A stream that you can write data to, it can go to any device that bytes of sequence can be transfered"/>
+<node CREATED="1409314571501" ID="ID_1964716928" MODIFIED="1409314588702" TEXT="Input Stream: A stream that you can read data from"/>
+</node>
+<node CREATED="1409314902615" ID="ID_1163278480" MODIFIED="1409314913147" TEXT="Binary and Character Stream"/>
+</node>
 <node CREATED="1409222058971" ID="ID_392967502" MODIFIED="1409222090916" POSITION="right" STYLE="bubble" TEXT="The classes for input and output"/>
 <node CREATED="1409222076225" ID="ID_697719747" MODIFIED="1409222094178" POSITION="right" STYLE="bubble" TEXT="The standard streams"/>
 </node>
