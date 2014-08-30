6ca146e1437f09aea64e04bc8f80bb74bf829420
Ruiyuan Li <lry198010@gmail.com>
2014-08-30 17:09:37 +0800

    finished code for example TryFormattedInput.java in section4 chapter8

--- chapter8/FormattedInput.java
+++ chapter8/FormattedInput.java
@@ -4,12 +4,77 @@ import java.io.StreamTokenizer;
 import java.io.IOException;
 
 public class FormattedInput {
-  private int readToken(){
-    ttype = 0;
-    try{
-      if
+  public void resetToken(){
+    tokenizer.resetSyntax();
+    bSyntax = true;
+  }
+  
+  public String readAllCh() throws InvalidUserInputException{
+    if(bSyntax){
+      StringBuffer s = new StringBuffer(""); 
+      int type;
+      while((type = readToken()) != tokenizer.TT_EOL){
+        s.append((char)type);
+      }
+      return s.toString();
+    }else{
+      throw new InvalidUserInputException("The resetSyntax is not set!");
+    } 
+  }
+
+  public double readDouble() throws InvalidUserInputException{
+    int type = readToken();
+    if(type == tokenizer.TT_NUMBER){
+      return tokenizer.nval;
+    }else if(type == tokenizer.TT_WORD){
+      throw new InvalidUserInputException("It is a Word token[not double]: " + tokenizer.sval, "" + tokenizer.sval);
+    }else{
+      throw new InvalidUserInputException("It in the end of Stream");
     }
   }
-  StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader (new InputStreamReader(System.io)));
-  private int ttype;
+
+  public String readStr() throws InvalidUserInputException{
+    int type = readToken();
+    if(type == tokenizer.TT_WORD || type == '\'' || type == '\"'){
+      return tokenizer.sval;
+    }else if(type == tokenizer.TT_NUMBER){
+      throw new InvalidUserInputException("It is a Number token[not String]: " + tokenizer.nval, "" + tokenizer.nval);
+    }else if(type == tokenizer.TT_EOF){
+      throw new InvalidUserInputException("It in the end of Stream");
+    }else{
+      throw new InvalidUserInputException("Unknown input error");
+    }
+  }
+
+  public int readInt() throws InvalidUserInputException{
+    int type = readToken();
+    if(type == StreamTokenizer.TT_NUMBER){
+      if(tokenizer.nval != (int)tokenizer.nval){
+        throw new InvalidUserInputException("It is a double token[not integer]:" + tokenizer.nval, "" + tokenizer.nval);
+      } 
+      return (int)tokenizer.nval;
+    }else{
+      if(type == StreamTokenizer.TT_WORD){
+        throw new InvalidUserInputException("It is a Word token[not number]:" + tokenizer.sval, tokenizer.sval);
+      }else{
+        throw new InvalidUserInputException("It in the end of Stream");
+      }
+    } 
+  }
+
+  private int readToken(){
+    int ttype = 0;
+    try{
+      if((ttype = tokenizer.nextToken()) == StreamTokenizer.TT_EOF){
+        System.out.println("In the End of Stream!");
+      }
+    }catch(IOException e){
+      e.printStackTrace();   
+      System.exit(1);
+    }
+    return ttype;
+  }
+  private StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader (new InputStreamReader(System.in)));
+  private boolean bSyntax = false; 
+  //private int ttype;
 } 
--- chapter8/TryFormattedInput.java
+++ chapter8/TryFormattedInput.java
@@ -1,3 +1,22 @@
 public class TryFormattedInput{
-
+  public static void main(String[] args){
+    FormattedInput f = new FormattedInput();
+    try{
+     /*
+      System.out.println("Please Enter an integer, a double, a string and a word:");
+      System.out.println("read Integer:" + f.readInt());
+      System.out.println("read double:" + f.readDouble());
+      System.out.println("read string:" + f.readStr());
+      System.out.println("read a word:" + f.readStr());
+     */ 
+      System.out.println("now reading the whole characters, please input the characters");
+      f.resetToken();
+      System.out.println("The input is:" + f.readAllCh());
+      System.out.println("End of the reading");
+    }catch(InvalidUserInputException e){
+      e.printStackTrace();
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
 }
