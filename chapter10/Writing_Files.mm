844652698b4850516d1abb1fefe8b923d38bd82f
Ruiyuan Li <lry198010@gmail.com>
2014-09-07 01:06:33 +0800

    start mm file for chapter10, Writing_Files.mm

---
+++ chapter10/Writing_Files.mm
@@ -0,0 +1,13 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1410022841121" ID="ID_1149928149" MODIFIED="1410022864678" TEXT="Writing Files">
+<node CREATED="1410022888545" ID="ID_769997669" MODIFIED="1410022933233" POSITION="right" STYLE="bubble" TEXT="File I/O Basics"/>
+<node CREATED="1410022935581" ID="ID_802655429" MODIFIED="1410023109832" POSITION="right" STYLE="bubble" TEXT="File Output"/>
+<node CREATED="1410022970341" ID="ID_579406167" MODIFIED="1410023116240" POSITION="right" STYLE="bubble" TEXT="Writing a File via an OutputStream"/>
+<node CREATED="1410022999775" ID="ID_1368063111" MODIFIED="1410023121736" POSITION="right" STYLE="bubble" TEXT="Writing a File Using Writer"/>
+<node CREATED="1410023020789" ID="ID_1642462474" MODIFIED="1410023125648" POSITION="right" STYLE="bubble" TEXT="Buffers"/>
+<node CREATED="1410023034263" ID="ID_1634736675" MODIFIED="1410023128805" POSITION="right" STYLE="bubble" TEXT="Writing a File Using a Channel"/>
+<node CREATED="1410023057503" ID="ID_626830264" MODIFIED="1410023131512" POSITION="right" STYLE="bubble" TEXT="File Write Operations"/>
+<node CREATED="1410023080677" ID="ID_875558697" MODIFIED="1410023136816" POSITION="right" STYLE="bubble" TEXT="Forcing Data to Be Writing to a Device"/>
+</node>
+</map>
