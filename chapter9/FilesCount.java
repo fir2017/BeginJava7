2c32e02d10ccf3ccc8fcd8ec6d57476c102f9397
Ruiyuan Li <lry198010@gmail.com>
2014-09-06 21:30:11 +0800

    finished code for quiz4, chapter9, T4.java and FilesCount.java

--- chapter9/FilesCount.java
+++ chapter9/FilesCount.java
@@ -1,27 +1,47 @@
-import static java.nio.file.VisitFileResult.*;
+import static java.nio.file.FileVisitResult.*;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
 import java.io.IOException;
 
-public class FilesCount extends SimpleFileVisitor {
+public class FilesCount extends SimpleFileVisitor <Path> {
   @Override
-  public FileVisitResult preVisitDirectory(Path dir,BasicFileAttributes attr) throws IOException {
+  public FileVisitResult preVisitDirectory(Path dir,BasicFileAttributes attr) {
+    System.err.println("Now enter:" + dir);
     currentDirFiles = 0;
+    return CONTINUE;
   } 
   
   @Override
-  public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException {
-    
+  public FileVisitResult postVisitDirectory(Path dir, IOException e) {
+    if(e == null){
+      System.err.println("\"" + dir + "\" contains: " + currentDirFiles);
+      System.err.println("Now leave:" + dir);
+      //currentDirFiles = 0;
+    }else{
+      e.printStackTrace();
+    } 
+    return CONTINUE;
   }
   
   @Override
-  public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
-
+  public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+    if(attr.isRegularFile() || attr.isSymbolicLink()){
+      totalFiles++;
+      currentDirFiles++;
+     }
+     return CONTINUE;
   }
   
   @Override 
-  public FileVisitResult visitFileFailed(Path file, IOException e) throws IOException {
-
+  public FileVisitResult visitFileFailed(Path file, IOException e) {
+    System.err.println("Error in:" + file);
+    e.printStackTrace();
+    //throw e;
+    return CONTINUE;
+  }
+  
+  public int getTotalFiles(){
+    return totalFiles;
   }
   
   private int totalFiles = 0;
--- chapter9/T4.java
+++ chapter9/T4.java
@@ -2,6 +2,53 @@ import java.nio.file.*;
 import java.io.IOException;
 public class T4 {
   public static void main (String[] args) {
-    
+    if(args.length <= 0){
+      System.err.println("Usage: java T4 dir");
+      System.err.println("Error: Directory required!");
+      return;
+    }    
+    try{
+      Path dir = Paths.get(args[0]);
+      int[] t = getFileDirs(dir);
+      System.err.println("Total directories:" + t[1]);
+      System.err.println("Total files:" + t[0]);
+      //FileVisitor<Path> fc = new FilesCount();
+      //Files.walkFileTree(dir,fc);
+      //fc = (FilesCount)fc;
+      //System.err.println(fc.getTotalFiles());
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+  public static int[] getFileDirs(Path dir) throws IOException{
+    int[] tmpResult = {0,0};
+    if(Files.isRegularFile(dir)){
+      tmpResult[0] = 1;
+    }else if(Files.isDirectory(dir)){
+      try(DirectoryStream<Path> paths = Files.newDirectoryStream(dir)){
+        int numFiles = 0;
+        for(Path p : paths) {
+          if(Files.isRegularFile(p)) numFiles++;
+          int[] t = getFileDirs(p);
+          tmpResult[0] += t[0];
+          tmpResult[1] += t[1];
+        }
+        System.err.println("\"" + dir + "\" contains: " + numFiles);
+        /*
+        for(Path p : paths) {
+          if(Files.isDirectory(p)){
+            int[] t = getFileDirs(p);
+            tmpResult[0] += t[0];
+            tmpResult[1] += t[1];
+          }
+        }
+        */
+        //tmpResult[0] += numFiles;
+        tmpResult[1]++;
+      }catch(IOException e){
+        throw e; 
+      }
+    }
+    return tmpResult;
   }
 }
