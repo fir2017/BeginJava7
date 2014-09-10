1645cf6e2f047ca57194c26941b04b909a295f32
Ruiyuan Li <lry198010@gmail.com>
2014-09-10 18:04:28 +0800

    finished code for the first example in section 7, chapter10, BufferStateTrace.java

---
+++ chapter10/BufferStateTrace.java
@@ -0,0 +1,37 @@
+import java.nio.*;
+import java.nio.file.*;
+import java.util.EnumSet;
+import java.nio.channels.*;
+import java.io.IOException;
+import static java.nio.file.StandardOpenOption.*;
+
+public class BufferStateTrace {
+  public static void main(String[] args) {
+    if(args.length <= 0) {
+      System.out.println("Error: File name required!");
+      return;
+    }
+    String txt = "Garbig in, Garbig out.";
+    Path fp = Paths.get(args[0]); 
+    fp = fp.toAbsolutePath();
+    try(WritableByteChannel fw = Files.newByteChannel(fp, WRITE, CREATE)){
+      ByteBuffer buf = ByteBuffer.allocate(1024);
+      System.out.printf("New Buffer: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      for(char ch : txt.toCharArray()){
+        buf.putChar(ch);
+      }
+      System.out.printf("Buffer After Load: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      buf.flip();
+      System.out.printf("Buffer flip: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      fw.write(buf);
+      System.out.printf("Buffer after write: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      buf.flip();
+      System.out.printf("Buffer flip again: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      fw.write(buf);
+      System.out.printf("Buffer after write second: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.println("Buffer write two times!");
+    }catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+} 
