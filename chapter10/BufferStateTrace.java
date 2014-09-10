30e70dad933e873038bdfa78fd7b50b878f79d1f
Ruiyuan Li <lry198010@gmail.com>
2014-09-10 18:10:02 +0800

    add filesize test to BufferStateTrace.java

--- chapter10/BufferStateTrace.java
+++ chapter10/BufferStateTrace.java
@@ -11,24 +11,24 @@ public class BufferStateTrace {
       System.out.println("Error: File name required!");
       return;
     }
-    String txt = "Garbig in, Garbig out.";
+    String txt = "Garbage in, Garbage out.";
     Path fp = Paths.get(args[0]); 
     fp = fp.toAbsolutePath();
     try(WritableByteChannel fw = Files.newByteChannel(fp, WRITE, CREATE)){
       ByteBuffer buf = ByteBuffer.allocate(1024);
-      System.out.printf("New Buffer: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("New Buffer: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       for(char ch : txt.toCharArray()){
         buf.putChar(ch);
       }
-      System.out.printf("Buffer After Load: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("Buffer After Load: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       buf.flip();
-      System.out.printf("Buffer flip: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("Buffer flip: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       fw.write(buf);
-      System.out.printf("Buffer after write: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("Buffer after write: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       buf.flip();
-      System.out.printf("Buffer flip again: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("Buffer flip again: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       fw.write(buf);
-      System.out.printf("Buffer after write second: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d%n", buf.position(),buf.limit(),buf.capacity());
+      System.out.printf("Buffer after write second: Position=%1$4d	Limit=%2$4d	Capacity=%3$4d	FileSize=%4$4d%n", buf.position(),buf.limit(),buf.capacity(),((FileChannel)fw).size());
       System.out.println("Buffer write two times!");
     }catch(Exception e) {
       e.printStackTrace();
